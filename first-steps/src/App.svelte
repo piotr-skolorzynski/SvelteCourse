047afef9912b6082212c85957d47228418a76e56
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-11-21 20:39:52 +0100

    add default props

--- first-steps/src/App.svelte
+++ first-steps/src/App.svelte
@@ -66,13 +66,13 @@
 <main>
   <form on:submit|preventDefault={addMeetup}>
 
-    <TextInput id="title" label="Title" type="text" value={title} on:input={event => (title = event.target.value)} />
+    <TextInput id="title" label="Title" value={title} on:input={event => (title = event.target.value)} />
 
-    <TextInput id="subtitle" label="Subtitle" type="text" value={subtitle} on:input={event => (subtitle = event.target.value)} />
+    <TextInput id="subtitle" label="Subtitle" value={subtitle} on:input={event => (subtitle = event.target.value)} />
 
-    <TextInput id="address" label="Address" type="text" value={address} on:input={event => (address = event.target.value)} />
+    <TextInput id="address" label="Address" value={address} on:input={event => (address = event.target.value)} />
 
-    <TextInput id="imageUrl" label="Image URL" type="text" value={imageUrl} on:input={event => (imageUrl = event.target.value)} />
+    <TextInput id="imageUrl" label="Image URL" value={imageUrl} on:input={event => (imageUrl = event.target.value)} />
 
     <TextInput id="email" label="E-Mail" type="email" value={email} on:input={event => (email = event.target.value)} />
 
