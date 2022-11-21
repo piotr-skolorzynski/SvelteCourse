c05d8234a927ed6e4bd4a5c489b3867e340e8416
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-11-21 22:51:14 +0100

    create Badge component and show how to use slot

--- first-steps/src/Meetups/MeetupItem.svelte
+++ first-steps/src/Meetups/MeetupItem.svelte
@@ -1,6 +1,7 @@
 <script>
   import { createEventDispatcher } from 'svelte';
   import Button from "../UI/Button.svelte";
+  import Badge from '../UI/Badge.svelte';
 
   export let id;
   export let title;
@@ -74,7 +75,11 @@
 
 <article>
     <header>
-        <h1>{title}</h1>
+        <h1>{title}
+          {#if isFavorite} 
+            <Badge>FAVORITE</Badge>
+          {/if}
+        </h1>
         <h2>{subtitle}</h2>
         <p>{address}</p>
     </header>
@@ -86,7 +91,16 @@
     </div>
     <footer>
       <Button href="mailto:{email}" caption="Contact"  />
-      <Button mode="outline" type="button" caption="Show Details" />
-      <Button type="button" caption={isFavorite ? 'Unfavorite' : 'Favorite'} on:click={()=> dispatch('toggleFavorite', id)} />
+      <Button 
+        type="button"
+        mode="outline"
+        color={isFavorite ? null : 'success'}
+        caption={isFavorite ? 'Unfavorite' : 'Favorite'} 
+        on:click={()=> dispatch('toggleFavorite', id)} 
+      />
+      <Button 
+        type="button" 
+        caption="Show Details" 
+      />
     </footer>
 </article>
